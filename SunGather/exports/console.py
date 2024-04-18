79c9ebcc76809708e72662c0169337817c6eeb35
ludifu <florian.unger@lehre.dhbw-stuttgart.de>
2024-04-18 08:41:45 +0200

    code formatting

--- SunGather/exports/console.py
+++ SunGather/exports/console.py
@@ -5,14 +5,22 @@ class export_console(object):
     # Configure Console
     def configure(self, config, inverter):
         print("+--------------------------------------------------------+")
-        print("| {:^54} |".format('Inverter Configuration Settings'))
+        print("| {:^54} |".format("Inverter Configuration Settings"))
         print("+--------------------------------------------------------+")
-        print("{:<30} {:<25} {:<1}".format("| " + 'Config',"| " + 'Value', "|"))
+        print("{:<30} {:<25} {:<1}".format("| " + "Config", "| " + "Value", "|"))
         print("+------------------------------+-------------------------+")
         for setting, value in inverter.client_config.items():
-            print("{:<30} {:<25} {:<1}".format("| " + str(setting), "| " + str(value), "|"))
+            print(
+                "{:<30} {:<25} {:<1}".format(
+                    "| " + str(setting), "| " + str(value), "|"
+                )
+            )
         for setting, value in inverter.inverter_config.items():
-            print("{:<30} {:<25} {:<1}".format("| " + str(setting), "| " + str(value), "|"))
+            print(
+                "{:<30} {:<25} {:<1}".format(
+                    "| " + str(setting), "| " + str(value), "|"
+                )
+            )
         print("+------------------------------+-------------------------+")
 
         return True
@@ -26,12 +34,21 @@ class export_console(object):
         bar = "+" + str.ljust("", table_width, "-") + "+"
 
         print(bar)
-        print("| {:<7} | ".format('Address') + str(str.ljust("Register", max_name_len)) + " | {:<27} |".format('Value'))
+        print(
+            "| {:<7} | ".format("Address")
+            + str(str.ljust("Register", max_name_len))
+            + " | {:<27} |".format("Value")
+        )
         print(bar)
         for register, value in inverter.latest_scrape.items():
-            print("| {:<7} | ".format(str(inverter.getRegisterAddress(register))) + str(str.ljust(register, max_name_len)) + " | {:<27} |".format(str(value) + " " + str(inverter.getRegisterUnit(register))))
+            print(
+                "| {:<7} | ".format(str(inverter.getRegisterAddress(register)))
+                + str(str.ljust(register, max_name_len))
+                + " | {:<27} |".format(
+                    str(value) + " " + str(inverter.getRegisterUnit(register))
+                )
+            )
         print(bar)
 
         print(f"Logged {len(inverter.latest_scrape)} registers to Console")
         return True
-
